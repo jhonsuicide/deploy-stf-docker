2ab4d4688785d874156248d3a4ec29d64613dce3
JOHANES INDRA ARTHUR SIRAIT <41463512+jhonsuicide@users.noreply.github.com>
2019-07-29 14:41:23 +0700

    Update deploy_stf.sh

--- deploy_stf.sh
+++ deploy_stf.sh
@@ -13,7 +13,7 @@
 # 
 #######################################################################################
 
-NETNAME="stf.wirecard"
+NETNAME=""
 NETWORK_INTERFACES=$(ls /sys/class/net)
 
 DNS_ADDRESS=$(nmcli device show ${NETNAME}|grep "IP4\.DNS\[1\]"|awk '{print $2}')
@@ -22,7 +22,7 @@ echo "DNS Address: ${DNS_ADDRESS}"
 # Get exported IP Adreess 
 [ ! -z "$(echo ${NETWORK_INTERFACES} | grep "wlo1")" ]&&NETNAME="wlo1"
 [ ! -z "$(echo ${NETWORK_INTERFACES} | grep "eno1")" ]&&NETNAME="eno1"
-IP_ADDRESS=$(ifconfig ${NETNAME}|grep "inet "|awk -F: '{print $2}'|awk '{print $1}')
+IP_ADDRESS=$(ip addr show ${NETNAME}|grep "inet "|awk -F: '{print $2}'|awk '{print $1}')
 [ $# -gt 0 ]&&IP_ADDRESS=$1
 echo "IP ADDRESS: ${IP_ADDRESS}"
 
